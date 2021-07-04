72096d42673db1492aea12000a4dc32e5ac8b75b
manoj kumar <manoj.choudhary@ncplinc.com>
2021-07-04 23:51:21 +0530

    new work update

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,19 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+	$(document).ready(function(){
+		alert('sdfsdfdsfsdf');
+	});
+	$(".fos_user_registration_register input").focus(function(){
+		$(this).addClass('is-focused');
+	});
+	$(".fos_user_registration_register input").focusout(function(){
+		$(this).removeClass('is-focused');
+	});
+	$('.fos_user_registration_register input').blur(function(){
+		tmpval = $(this).val();
+		if(tmpval == '' && !$(this).hasClass('whatsappinput')) {
+			$(this).addClass('is-empty');
+		} else {
+			$(this).removeClass('is-empty');
+		}
+	});
